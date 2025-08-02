c87a9fe8d61959147208d1950e1683c71993fcd6
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-08-02 17:11:43 +0200

    refactor(print_scripts) enhance script listing functionality with folder filtering

--- print_scripts.py
+++ print_scripts.py
@@ -1,3 +1,14 @@
 """ FILE TEMPLATE """
 
 scripts = [] # Will be populated with script identifiers
+
+def print_scripts(folder: str = ""):
+	head = ""
+	print("Available scripts:")
+	for script in scripts:
+		script_group, script_name = script.split("-", 1)
+		if folder and script_group != folder: continue
+		if script_group != head:
+			head = script_group
+			print(f"\n{head.upper()}")
+		print(f"{script_name}")
