77f5d8baf05387d2d9550416ca47768024a3bf62
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-08-02 16:47:41 +0200

    add(compile_all) list_scripts functions

--- compile_all.py
+++ compile_all.py
@@ -1,5 +1,14 @@
 from pathlib import Path
+from typing import Generator
 
 SCRIPT = Path(__file__)
 SCRIPT_FOLDER = Path(__file__).parent
 COMPILE_FOLDER = SCRIPT_FOLDER / "compiled"
+
+def list_scripts() -> Generator[tuple[Path, str], None, None]:
+	"""Lists all Python scripts in the current directory."""
+	for file in SCRIPT_FOLDER.glob("**/*.py"):
+		if not file.is_file(): continue # Ensure it's a file
+		if file.name == SCRIPT.name: continue # Skip the current script
+		if file.parent.name == "compiled": continue # Skip compiled scripts
+		yield (file, f"{file.parent.name}-{file.stem}")
